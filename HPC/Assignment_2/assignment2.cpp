1d28914667acea0a338198e73d840d025c76d16c
gitSarvesh <127534419+gitSarvesh@users.noreply.github.com>
2025-03-04 12:08:08 +0530

    Update assignment2.cpp

--- HPC/Assignment_2/assignment2.cpp
+++ HPC/Assignment_2/assignment2.cpp
@@ -1,181 +1,167 @@
 #include<iostream>
 #include<omp.h>
 #include<bits/stdc++.h>
+#include<chrono>
 
 using namespace std;
 
+void minimum(vector<int> array){
+    int min = INT_MAX;
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    cout<<"Sequential Min"<<endl;
+    start = std::chrono::system_clock::now();
+    for(auto i = array.begin(); i != array.end();i++){
+        if(*i < min){
+            min = *i;
+        } 
+    }
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds_sqmin = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    cout << endl;
+    cout << "Minimum Element: " << min << endl;
+    cout << endl;
+    std::cout << "finished computation at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds_sqmin.count() << "s\n";
+              
+    cout<<"Parallel Min"<<endl;
+    int min_ele = INT_MAX;
+    start = std::chrono::system_clock::now();
+    #pragma omp parallel for reduction(min: min_ele)
+        for(auto i = array.begin(); i != array.end();i++){
+            if(*i < min_ele){
+                min_ele = *i;
+            } 
+        }
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds_plmin = end - start;
+    std::time_t end_time_t = std::chrono::system_clock::to_time_t(end);
+ 
+    cout << endl;
+    std::cout << "finished computation at " << std::ctime(&end_time_t)
+              << "elapsed time: " << elapsed_seconds_plmin.count() << "s\n";
+    cout << "Minimum Element(Parallel Reduction): " << min_ele << endl;
+    cout<<"Speed Factor = "<<elapsed_seconds_sqmin.count()/elapsed_seconds_plmin.count()<<endl;
+}
 
+void maximum(vector<int> array){
+    int max = INT_MIN;
+    cout<<"Sequential Max"<<endl;
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
+    for(auto i = array.begin(); i != array.end();i++){
+        if(*i > max){
+            max = *i;
+        } 
+    }
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds_sqmax = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    cout << "Maximum Element: " << max << endl;
+    cout << endl;
+    std::cout << "finished computation at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds_sqmax.count() << "s\n";
     
-
-
-
-void sequential_bubble_sort(int arr[],int size){
-    int array[size];
-    for(int i = 0 ; i < size; i++){
-        array[i] = arr[i];
-    }
-
-    double start = omp_get_wtime();
-    for(int i = 0; i < size - 1; i ++){
-        for(int j = 0; j < size - i - 1; j++){
-            if(array[j] > array[j+1]){
-                swap(array[j],array[j+1]);
-            }
+    cout<<"Parallel Max"<<endl;
+    int max_ele = INT_MIN;
+    start = std::chrono::system_clock::now();
+    #pragma omp parallel for reduction(max: max_ele)
+        for(auto i = array.begin(); i != array.end();i++){
+            if(*i > max_ele){
+                max_ele = *i;
+            } 
         }
-    }
-    double end = omp_get_wtime();
-    cout << "Sequential Bubble Sort:\n";
-    // for(int i = 0 ; i < size; i++){
-    //     cout << array[i] << " ";
-    // }
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds_plmax = end - start;
+    std::time_t end_time_t = std::chrono::system_clock::to_time_t(end);
+ 
     cout << endl;
-    cout << "Time Required: " << end - start << endl;
-
+    std::cout << "finished computation at " << std::ctime(&end_time_t)
+              << "elapsed time: " << elapsed_seconds_plmax.count() << "s\n";
+    cout << "Maximum Element(Parallel Reduction): " << max_ele << endl;
+    cout<<"Speed Factor = "<<elapsed_seconds_sqmax.count()/elapsed_seconds_plmax.count()<<endl;
+    
 }
 
-void parallel_bubble_sort(int arr[],int size){
-    int array[size];
-    for(int i = 0 ; i < size; i++){
-        array[i] = arr[i];
+void sum(vector<int> array){
+    int sum = 0;
+    cout<<"Sequential Sum"<<endl;
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
+    for(auto i = array.begin(); i != array.end();i++){
+        sum += *i; 
     }
-    double start = omp_get_wtime();
-    for(int k = 0; k < size;k ++){
-        if(k % 2 == 0){
-            #pragma omp parallel for
-                for(int i = 1; i < size - 1; i += 2){
-                    if(array[i] > array[i+1]){
-                        swap(array[i],array[i+1]);
-                    }
-                }
-        }
-        else{
-            #pragma omp parallel for
-                for(int i = 0; i < size - 1; i += 2){
-                    if(array[i] > array[i+1]){
-                        swap(array[i],array[i+1]);
-                    }
-                }
-        }
-    }
-    double end = omp_get_wtime();
-    cout << "Parallel Bubble Sort:\n";
-    // for(int i = 0 ; i < size; i++){
-    //     cout << array[i] << " ";
-    // }
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds_sqsum = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    cout << "Summation: " << sum << endl;
     cout << endl;
-    cout << "Time Required: " << end - start << endl;
-}
-
-void merge(int array[],int low, int mid, int high,int size){
-    int temp[size];
-    int i = low;
-    int j = mid + 1;
-    int k = 0;
-    while((i <= mid) && (j <= high)){
-        if(array[i] >= array[j]){
-            temp[k] = array[j];
-            k++;
-            j++;
+    std::cout << "finished computation at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds_sqsum.count() << "s\n"; 
+    
+    cout<<"Parallel Sum"<<endl;
+    sum = 0;
+    start = std::chrono::system_clock::now();
+    #pragma omp parallel for reduction(+: sum)
+        for(auto i = array.begin(); i != array.end();i++){
+            sum += *i; 
         }
-        else{
-            temp[k] = array[i];
-            k++;
-            i++;
-        }
-    }
-    while(i <= mid){
-        temp[k] = array[i];
-        k++;
-        i++;
-    }
-    while(j <= high){
-        temp[k] = array[j];
-        k++;
-        j++;
-    }
-
-    k = 0;
-    for(int i = low;i <= high;i++){
-        array[i] = temp[k];
-        k++;
-    }
-}
-
-void mergesort(int array[],int low,int high,int size){
-    if(low < high){
-        int mid = (low + high) / 2;
-        mergesort(array,low,mid,size);
-        mergesort(array,mid+1,high,size);
-        merge(array,low,mid,high,size);
-    }
-}
-
-void perform_merge_sort(int arr[],int size){
-    int array[size];
-    for(int i = 0 ; i < size; i++){
-        array[i] = arr[i];
-    }
-    double start = omp_get_wtime();
-    mergesort(array,0,size-1,size);
-    double end = omp_get_wtime();
-    cout << "Merge Sort:\n";
-    // for(int i = 0 ; i < size; i++){
-    //     cout << array[i] << " ";
-    // }
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds_plsum = end - start;
+    std::time_t end_time_t = std::chrono::system_clock::to_time_t(end);
+ 
     cout << endl;
-    cout << "Time Required: " << end - start << endl;
+    std::cout << "finished computation at " << std::ctime(&end_time_t)
+              << "elapsed time: " << elapsed_seconds_plsum.count() << "s\n";
+    cout << "Summation(Parallel Reduction): " << sum << endl;
+    cout<<"Speed Factor = "<<elapsed_seconds_sqsum.count()/elapsed_seconds_plsum.count()<<endl;
 }
-
-void p_mergesort(int array[],int low,int high,int size){
-    if(low < high){
-        int mid = (low + high) / 2;
-        #pragma omp parallel sections
-        {
-            #pragma omp section
-                p_mergesort(array,low,mid,size);
-            #pragma omp section
-                p_mergesort(array,mid+1,high,size);
-        }
-        merge(array,low,mid,high,size);
+void average(vector<int> array){
+    float avg = 0;
+    cout<<"Sequential Avg"<<endl;
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
+    for(auto i = array.begin(); i != array.end();i++){
+        avg += *i; 
     }
-}
-
-void perform_p_merge_sort(int arr[],int size){
-    int array[size];
-    for(int i = 0 ; i < size; i++){
-        array[i] = arr[i];
-    }
-    double start = omp_get_wtime();
-    p_mergesort(array,0,size-1,size);
-    double end = omp_get_wtime();
-    cout << "Parallel Merge Sort:\n";
-    // for(int i = 0 ; i < size; i++){
-    //     cout << array[i] << " ";
-    // }
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds_sqavg = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    cout << "Average: " << avg / array.size() << endl;
     cout << endl;
-    cout << "Time Required: " << end - start << endl;
+    std::cout << "finished computation at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds_sqavg.count() << "s\n"; 
+    
+    cout<<"Parallel Avg"<<endl;
+    avg = 0;
+    start = std::chrono::system_clock::now();
+    #pragma omp parallel for reduction(+: avg)
+        for(auto i = array.begin(); i != array.end();i++){
+            avg += *i; 
+        }
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds_plavg = end - start;
+    std::time_t end_time_t = std::chrono::system_clock::to_time_t(end);
+ 
+    cout << endl;
+    std::cout << "finished computation at " << std::ctime(&end_time_t)
+              << "elapsed time: " << elapsed_seconds_plavg.count() << "s\n";
+    cout << "Average(Parallel Reduction): " << avg / array.size() << endl;
+    cout<<"Speed Factor = "<<elapsed_seconds_sqmin.count()/elapsed_seconds_plmin.count()<<endl;
 }
 
-
-
-int main(int argc, char const *argv[])
-{
-    int SIZE;
+int main(){
+    cout << "Enter number of elements in array: ";
+    int N;
     int MAX = 1000;
-    cout << "Enter size of array: ";
-    cin >> SIZE;
-    int array[SIZE];
-    for(int i = 0 ; i < SIZE; i ++){
-        array[i] = rand() % MAX;
+    cin >> N;
+    vector<int> array;
+    for(int i = 0; i < N; i++){
+        array.push_back(rand() % MAX);
     }
-    // cout << "Initial Array:\n";
-    // for(int i = 0 ; i < SIZE; i++){
-    //     cout << array[i] << " ";
-    // }
-    cout << endl;
-    sequential_bubble_sort(array,SIZE);
-    parallel_bubble_sort(array,SIZE);
-    perform_merge_sort(array,SIZE);
-    perform_p_merge_sort(array,SIZE);
+    minimum(array);
+    maximum(array);
+    sum(array);
+    average(array);
     return 0;
 }
