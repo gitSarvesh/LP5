ee2b5103311cd0b00378dd45ebc1882efaa208d4
gitSarvesh <127534419+gitSarvesh@users.noreply.github.com>
2025-03-04 12:06:26 +0530

    Update assignment3.cpp

--- HPC/Assignment_3/assignment3.cpp
+++ HPC/Assignment_3/assignment3.cpp
@@ -1,98 +1,153 @@
 #include<iostream>
 #include<omp.h>
 #include<bits/stdc++.h>
+#include<chrono>
 
 using namespace std;
 
 void minimum(vector<int> array){
     int min = INT_MAX;
-    double start = omp_get_wtime();
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    cout<<"Sequential Min"<<endl;
+    start = std::chrono::system_clock::now();
     for(auto i = array.begin(); i != array.end();i++){
         if(*i < min){
             min = *i;
         } 
     }
-    double end = omp_get_wtime();
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds_sqmin = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    cout << endl;
     cout << "Minimum Element: " << min << endl;
-    cout << "Time Taken: " << (end-start) << endl; 
+    cout << endl;
+    std::cout << "finished computation at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds_sqmin.count() << "s\n";
+              
+    cout<<"Parallel Min"<<endl;
     int min_ele = INT_MAX;
-    start = omp_get_wtime();
+    start = std::chrono::system_clock::now();
     #pragma omp parallel for reduction(min: min_ele)
         for(auto i = array.begin(); i != array.end();i++){
             if(*i < min_ele){
                 min_ele = *i;
             } 
         }
-    end = omp_get_wtime();
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds_plmin = end - start;
+    std::time_t end_time_t = std::chrono::system_clock::to_time_t(end);
+ 
+    cout << endl;
+    std::cout << "finished computation at " << std::ctime(&end_time_t)
+              << "elapsed time: " << elapsed_seconds_plmin.count() << "s\n";
     cout << "Minimum Element(Parallel Reduction): " << min_ele << endl;
-    cout << "Time Taken: " << (end-start) << endl;
-    
+    cout<<"Speed Factor = "<<elapsed_seconds_sqmin.count()/elapsed_seconds_plmin.count()<<endl;
 }
 
 void maximum(vector<int> array){
     int max = INT_MIN;
-    double start = omp_get_wtime();
+    cout<<"Sequential Max"<<endl;
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
     for(auto i = array.begin(); i != array.end();i++){
         if(*i > max){
             max = *i;
         } 
     }
-    double end = omp_get_wtime();
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds_sqmax = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
     cout << "Maximum Element: " << max << endl;
-    cout << "Time Taken: " << (end-start) << endl; 
+    cout << endl;
+    std::cout << "finished computation at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds_sqmax.count() << "s\n";
+    
+    cout<<"Parallel Max"<<endl;
     int max_ele = INT_MIN;
-    start = omp_get_wtime();
+    start = std::chrono::system_clock::now();
     #pragma omp parallel for reduction(max: max_ele)
         for(auto i = array.begin(); i != array.end();i++){
             if(*i > max_ele){
                 max_ele = *i;
             } 
         }
-    end = omp_get_wtime();
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds_plmax = end - start;
+    std::time_t end_time_t = std::chrono::system_clock::to_time_t(end);
+ 
+    cout << endl;
+    std::cout << "finished computation at " << std::ctime(&end_time_t)
+              << "elapsed time: " << elapsed_seconds_plmax.count() << "s\n";
     cout << "Maximum Element(Parallel Reduction): " << max_ele << endl;
-    cout << "Time Taken: " << (end-start) << endl;
+    cout<<"Speed Factor = "<<elapsed_seconds_sqmax.count()/elapsed_seconds_plmax.count()<<endl;
     
 }
 
 void sum(vector<int> array){
     int sum = 0;
-    double start = omp_get_wtime();
+    cout<<"Sequential Sum"<<endl;
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
     for(auto i = array.begin(); i != array.end();i++){
         sum += *i; 
     }
-    double end = omp_get_wtime();
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds_sqsum = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
     cout << "Summation: " << sum << endl;
-    cout << "Time Taken: " << (end-start) << endl; 
+    cout << endl;
+    std::cout << "finished computation at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds_sqsum.count() << "s\n"; 
+    
+    cout<<"Parallel Sum"<<endl;
     sum = 0;
-    start = omp_get_wtime();
+    start = std::chrono::system_clock::now();
     #pragma omp parallel for reduction(+: sum)
         for(auto i = array.begin(); i != array.end();i++){
             sum += *i; 
         }
-    end = omp_get_wtime();
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds_plsum = end - start;
+    std::time_t end_time_t = std::chrono::system_clock::to_time_t(end);
+ 
+    cout << endl;
+    std::cout << "finished computation at " << std::ctime(&end_time_t)
+              << "elapsed time: " << elapsed_seconds_plsum.count() << "s\n";
     cout << "Summation(Parallel Reduction): " << sum << endl;
-    cout << "Time Taken: " << (end-start) << endl;
-    
+    cout<<"Speed Factor = "<<elapsed_seconds_sqsum.count()/elapsed_seconds_plsum.count()<<endl;
 }
 void average(vector<int> array){
     float avg = 0;
-    double start = omp_get_wtime();
+    cout<<"Sequential Avg"<<endl;
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
     for(auto i = array.begin(); i != array.end();i++){
         avg += *i; 
     }
-    double end = omp_get_wtime();
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds_sqavg = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
     cout << "Average: " << avg / array.size() << endl;
-    cout << "Time Taken: " << (end-start) << endl; 
+    cout << endl;
+    std::cout << "finished computation at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds_sqavg.count() << "s\n"; 
+    
+    cout<<"Parallel Avg"<<endl;
     avg = 0;
-    start = omp_get_wtime();
+    start = std::chrono::system_clock::now();
     #pragma omp parallel for reduction(+: avg)
         for(auto i = array.begin(); i != array.end();i++){
             avg += *i; 
         }
-    end = omp_get_wtime();
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds_plavg = end - start;
+    std::time_t end_time_t = std::chrono::system_clock::to_time_t(end);
+ 
+    cout << endl;
+    std::cout << "finished computation at " << std::ctime(&end_time_t)
+              << "elapsed time: " << elapsed_seconds_plavg.count() << "s\n";
     cout << "Average(Parallel Reduction): " << avg / array.size() << endl;
-    cout << "Time Taken: " << (end-start) << endl;
-    
+    cout<<"Speed Factor = "<<elapsed_seconds_sqmin.count()/elapsed_seconds_plmin.count()<<endl;
 }
 
 int main(){
